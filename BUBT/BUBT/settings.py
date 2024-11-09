9ea92fe3f6ad8137402dbe0cb568c2fdb43e8e79
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2024-11-09 14:04:19 +0600

    create academic_utils app and make three model

--- BUBT/BUBT/settings.py
+++ BUBT/BUBT/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'academic_utils',
 ]
 
 MIDDLEWARE = [
@@ -51,7 +52,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'academic_utils',
 ]
 
 ROOT_URLCONF = 'BUBT.urls'
--- BUBT/academic_utils/admin.py
+++ BUBT/academic_utils/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import AcademicSemester, AcademicFaculty, AcademicProgram
 
 
-@admin.site.register(AcademicSemester)
+@admin.register(AcademicSemester)
 class AcademicSemesterAdmin(admin.ModelAdmin):
     list_display = (
         'name',
@@ -16,14 +16,14 @@ class AcademicSemesterAdmin(admin.ModelAdmin):
     ordering = ('-created_at',)
 
 
-@admin.site.register(AcademicFaculty)
+@admin.register(AcademicFaculty)
 class AcademicFacultyAdmin(admin.ModelAdmin):
     list_display = ('name', 'created_at', 'updated_at')
     search_fields = ('name',)
     ordering = ('-created_at',)
 
 
-@admin.site.register(AcademicProgram)
+@admin.register(AcademicProgram)
 class AcademicProgramAdmin(admin.ModelAdmin):
     list_display = (
         'name', 'eke_name', 'academic_faculty'
