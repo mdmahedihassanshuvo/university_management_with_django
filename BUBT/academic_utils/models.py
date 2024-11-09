976355efc4c8179a64753a6259f57290d39665c9
mdmahedihassanshuvo <mdmahedihassanshuvo8@gmail.com>
2024-11-09 17:08:40 +0600

    student model added

--- BUBT/academic_utils/models.py
+++ BUBT/academic_utils/models.py
@@ -7,8 +7,8 @@ class AcademicSemester(models.Model):
     code = models.CharField(max_length=1)
     start_month = models.DateField()
     end_month = models.DateField()
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -16,8 +16,8 @@ class AcademicSemester(models.Model):
 
 class AcademicFaculty(models.Model):
     name = models.CharField(max_length=100, unique=True)
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
@@ -30,8 +30,8 @@ class AcademicProgram(models.Model):
         AcademicFaculty,
         on_delete=models.CASCADE
     )
-    created_at = models.DateField(auto_now_add=True)
-    updated_at = models.DateField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
---
+++ BUBT/student/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from academic_utils.models import AcademicSemester
+
+
+class Student(models.Model):
+    GENDER_CHOICES = [
+        ('male', 'MALE'),
+        ('female', 'FEMALE'),
+        ('other', 'OTHER')
+    ]
+    GUARDIAN_CHOICES = [
+        ('father', 'FATHER'),
+        ('mother', 'MOTHER'),
+        ('other', 'OTHER')
+    ]
+    name = models.CharField(max_length=50)
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICES)
+    date_of_birth = models.DateField()
+    email = models.EmailField(max_length=100, unique=True)
+    contact_no = models.CharField(max_length=20)
+    present_address = models.CharField(max_length=100)
+    permanent_address = models.CharField(max_length=100)
+    guardian = models.CharField(max_length=6, choices=GUARDIAN_CHOICES)
+    guardian_name = models.CharField(max_length=50)
+    profile_image = models.ImageField(upload_to='profile_image/student/')
+    academic_semester = models.ForeignKey(
+        AcademicSemester,
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
